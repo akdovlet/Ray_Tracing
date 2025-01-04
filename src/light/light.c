df3f1d14b642c174fef51fab64ab2cbbaccf4c89
Akhmed Dovletov <akdovlet@bess-f4r2s8.clusters.42paris.fr>
2025-01-04 10:47:16 +0100

    FEATURE: Blinn-Phong lighting

--- src/light/light.c
+++ src/light/light.c
@@ -6,7 +6,7 @@
 /*   By: akdovlet <akdovlet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/01 19:46:16 by akdovlet          #+#    #+#             */
-/*   Updated: 2025/01/03 19:21:42 by akdovlet         ###   ########.fr       */
+/*   Updated: 2025/01/04 10:45:12 by akdovlet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,11 +34,16 @@ t_tuple	lighting(t_material mat, t_light light, t_tuple eyev, t_tuple normalv, t
 	float	reflect_dot_eye;
 	float	light_dot_normal;
 	float	factor;
+	float	energy_conservation;
 
 	e_color = color_hadamard(mat.color, light.intensity);
 	lightv = tuple_normalize(tuple_substract(light.position, point));
 	ambient = tuple_multiply(e_color, mat.ambient);
 	light_dot_normal = tuple_dot(lightv, normalv);
+	halfway = tuple_normalize(tuple_add(lightv, eyev));
+	energy_conservation = (8.0 + 16.0) / (8.0 * M_PI);
+	factor = powf(fmax(tuple_dot(normalv, halfway), 0.0), mat.shininess);
+	factor = energy_conservation * factor;
 	if (light_dot_normal < 0)
 	{
 		diffuse = color_new(0, 0, 0);
@@ -50,21 +55,18 @@ t_tuple	lighting(t_material mat, t_light light, t_tuple eyev, t_tuple normalv, t
 		diffuse = tuple_multiply(tmp, light_dot_normal);
 		reflectv = reflect(tuple_negate(lightv), normalv);
 		reflect_dot_eye = tuple_dot(reflectv, eyev);
-		halfway = tuple_normalize(tuple_add(lightv, eyev));
 		// printf("reflect_dot_eye is: %f\n", reflect_dot_eye);
 		if (reflect_dot_eye <= 0.0f)
 			specular = color_new(0, 0, 0);
 		else
 		{
 			// printf("reflect.eye is: %f and mat.shininess is: %f\n", reflect_dot_eye, mat.shininess);
-			factor = powf(reflect_dot_eye, mat.shininess);
 			// if (reflect_dot_eye >=1)
 			// 	printf("factor is: %f\n", factor);
 			specular = tuple_multiply(light.intensity, mat.specular);
 			specular = tuple_multiply(specular, reflect_dot_eye);
-			// specular = tuple_multiply(specular, factor);
+			specular = tuple_multiply(specular, factor);
 		}
 	}
-	return (lightv);
-	
+	return (tuple_add(tuple_add(diffuse, ambient), specular));
 }
